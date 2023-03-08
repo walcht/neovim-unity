590a00c1d3d29276985faffe9fbbfe88e1c7948a
walid chtioui <walid.chtioui.main@gmail.com>
2023-03-08 21:14:40 +0100

    fix keymap issues & add new keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -27,7 +27,7 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "<C-sh>", function() vim.lsp.buf.signature_help() end, opts)
 		
 end)
 
--- lua/walcht/keymaps.lua
+++ lua/walcht/keymaps.lua
@@ -5,13 +5,14 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- Shorten function name
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 -- Leader key remapping
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Normal Mode --
 -- Window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -26,21 +27,48 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bprevious<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text vertically in noraml mode
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
--- Visual --
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- Disable Q key
+keymap("n", "Q", "<nop>")
+
+-- Copy to system clipboard
+keymap("n", "<leader>y", "\"+y")
+keymap("n", "<leader>Y", "\"+Y")
+
+-- Deleting to void register
+-- Normally, deleting a sequence results in that sequence overriding whatever
+-- is previously yanked. This behaviour is really confusing thus by using the
+-- below keymap, this behaviour is disabled.
+keymap("n", "<leader>d", "\"_d")
+
+-- Replace the current word pointed by cursor
+keymap("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Visual Mode --
 -- Stauy in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+-- Move text up and down and adjust indents accordingly
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Copy to system clipboard
+keymap("v", "<leader>y", "\"+y")
+
+-- Deleting to void register
+keymap("v", "<leader>d", "\"_d")
 
 -- Visual block --
 -- Move text up and down --
@@ -48,6 +76,9 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Disabling bad vim paste behaviour
+keymap("x", "<leader>p", "\"_dP")
 
 -- Terminal --
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
