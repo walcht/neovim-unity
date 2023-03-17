26a418cec18577d6f20aebc1275bbe9af7f6c225
walid chtioui <walid.chtioui.main@gmail.com>
2023-03-17 18:05:13 +0100

    feat: Add on_attach for possible server-specific config

--- plugin/lsp_related/lspconfig.lua
+++ plugin/lsp_related/lspconfig.lua
@@ -57,12 +57,40 @@ mason.setup {
 -- Add your language server(s) here
 local servers = {}
 
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
 -- Default config for all language servers
 local default_config = {
+    on_attach = on_attach,
     capabilities = capabilities,
 }
 
+-- Configuration for omnisharp
+-- Why not csharp-ls? I faced tons of issues working with that ls on Ubuntu
+-- and omnisharp seems (at least currently) to have a much better support.
 servers['omnisharp'] = {
+    on_attach = on_attach,
     capabilities = capabilities,
     handlers = {
         ["textDocument/definition"] = require('omnisharp_extended').handler,
@@ -72,6 +100,7 @@ servers['omnisharp'] = {
 }
 
 servers['lua_ls'] = {
+    on_attach = on_attach,
     capabilities = capabilities,
     settings = {
         Lua = {
