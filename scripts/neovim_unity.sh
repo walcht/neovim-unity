c59c09e07095be3692943e11518e467516cf84fb
walid chtioui <walid.chtioui.main@gmail.com>
2023-03-21 01:12:02 +0100

    feat(unity_script): Focus on nvim instance using wmctrl

--- scripts/neovim_unity.sh
+++ scripts/neovim_unity.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gnome-terminal -- nvr --servername "/tmp/nvimunity" --remote-silent $@
+gnome-terminal --title="nvimunity" -- nvr --servername \
+    "/tmp/nvimunity" --remote-tab-silent $@ && \
+    if command -v wmctrl > /dev/null; then wmctrl -F -a nvimunity; fi
