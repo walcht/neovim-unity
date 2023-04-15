e25f29b92f1b38f88ef34d7935f942e28f22c675
walid chtioui <walid.chtioui.main@gmail.com>
2023-04-15 18:39:17 +0200

    fix(linting): Use black for Python formatting instead of autopep8

--- plugin/lsp_related/null_ls.lua
+++ plugin/lsp_related/null_ls.lua
@@ -7,18 +7,18 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local completion = null_ls.builtins.completion
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
     sources = {
         -- Python
-        formatting.autopep8,
+        formatting.black.with({
+            extra_args = {"--line-length=79"}
+        }),
         diagnostics.flake8,
 
         -- C#
         formatting.csharpier,
-
-        -- Lua
-        formatting.stylua,
 
         -- Markdown
         diagnostics.markdownlint,
@@ -27,7 +27,9 @@ null_ls.setup({
         diagnostics.jsonlint,
 
         -- Misc
-        diagnostics.cspell,
-        completion.spell,
+        diagnostics.cspell.with({
+            filetypes = {"csharp", "markdown", "python"}
+        }),
+        code_actions.cspell,
     },
 })
