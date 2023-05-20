fd7833d3607cb59e624ef93f2bece6a553d68dc1
walid chtioui <walid.chtioui.main@gmail.com>
2023-05-20 04:41:22 +0200

    fix(formatting): Fix formatting error on save

--- after/plugin/lsp_related/null_ls.lua
+++ after/plugin/lsp_related/null_ls.lua
@@ -37,6 +37,7 @@ null_ls.setup({
         }),
         code_actions.cspell,
     },
+
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -44,7 +45,8 @@ null_ls.setup({
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.formatting_sync()
+
+                    vim.lsp.buf.format()
                 end,
             })
         end
