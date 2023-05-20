1189ff5efecb63cef63b61a818db884433495183
walid chtioui <walid.chtioui.main@gmail.com>
2023-05-20 04:30:25 +0200

    feat(formatter): Add formatting-on-save hook

--- after/plugin/lsp_related/null_ls.lua
+++ after/plugin/lsp_related/null_ls.lua
@@ -6,6 +6,7 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 -- before adding any new linters/formatters, make sure that you add them
 -- to Mason's list of linters_and_formatters. That way they are ensured to
 -- be installed and managed through Mason!
@@ -36,4 +37,16 @@ null_ls.setup({
         }),
         code_actions.cspell,
     },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
 })
