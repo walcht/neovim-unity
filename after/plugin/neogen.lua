33f844d25bcd66f9fe6e5e3736a7dc405b5591b8
walid chtioui <walid.chtioui.main@gmail.com>
2023-09-08 01:03:52 +0200

    feat(Python): Use numpy annotations for Python

--- after/plugin/neogen.lua
+++ after/plugin/neogen.lua
@@ -3,11 +3,13 @@ if not status_ok then
     print('Plugin not loaded: ', 'neogen')
     return
 end
-
 local opts = { noremap = true, silent = true }
+---------------------------------------------------- KEYMAPS ------------------------------------------------
 vim.api.nvim_set_keymap("n", "<Leader>af", ":lua require('neogen').generate()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>ac", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
-
+vim.api.nvim_set_keymap("n", "<Leader>at", ":lua require('neogen').generate({ type = 'type' })<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>aF", ":lua require('neogen').generate({ type = 'file' })<CR>", opts)
+-------------------------------------------------------------------------------------------------------------
 neogen.setup {
     snippet_engine = "luasnip",
     enabled = true,
@@ -19,7 +21,7 @@ neogen.setup {
         },
         python = {
             template = {
-                annotation_convention = "google_docstrings"
+                annotation_convention = "numpydoc"
             }
         }
     }
