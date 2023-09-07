53fd0a8dddc8cbc299ad54a3f3ea9e2280334f93
walid chtioui <walid.chtioui.main@gmail.com>
2023-09-08 00:17:52 +0200

    chore(Colortheme): Edit mocha colortheme

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -3,39 +3,93 @@ if not status_ok then
     print('Plugin not loaded: ', 'catppuccin')
     return
 end
-
 catppuccin.setup({
-    flavour = "frappe", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = {     -- :h background
         light = "latte",
         dark = "mocha",
     },
     transparent_background = true,
-    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-    term_colors = false,
+    show_end_of_buffer = true, -- show the '~' characters after the end of buffers
+    term_colors = true,
     dim_inactive = {
         enabled = false,
         shade = "dark",
         percentage = 0.15,
     },
     no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
+    no_bold = false,   -- Force no bold
     styles = {
         comments = { "italic" },
         conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
+        loops = { "italic" },
+        functions = { "bold" },
+        keywords = { "italic" },
         strings = {},
-        variables = {},
+        variables = { "bold" },
         numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
+        booleans = { "italic" },
+        properties = { "bold" },
+        types = { "bold" },
         operators = {},
     },
-    color_overrides = {},
-    custom_highlights = {},
+    color_overrides = {
+        mocha = {
+            rosewater = "#EA6962",
+            flamingo = "#EA6962",
+            pink = "#D3869B",
+            mauve = "#D3869B",
+            red = "#EA6962",
+            maroon = "#EA6962",
+            peach = "#BD6F3E",
+            yellow = "#D8A657",
+            green = "#A9B665",
+            teal = "#89B482",
+            sky = "#89B482",
+            sapphire = "#89B482",
+            blue = "#7DAEA3",
+            lavender = "#7DAEA3",
+            text = "#D4BE98",
+            subtext1 = "#BDAE8B",
+            subtext0 = "#A69372",
+            overlay2 = "#8C7A58",
+            overlay1 = "#735F3F",
+            overlay0 = "#95a5a6",  -- comment color
+            surface2 = "#4B4F51",
+            surface1 = "#2A2D2E",
+            surface0 = "#232728",
+            base = "#1D2021",
+            mantle = "#191C1D",
+            crust = "#151819",
+        },
+    },
+    custom_highlights = function(colors)
+        return {
+            NormalFloat = { bg = colors.crust },
+            FloatBorder = { bg = colors.crust, fg = colors.crust },
+            VertSplit = { bg = colors.base, fg = colors.surface0 },
+            CursorLineNr = { fg = colors.mauve, style = { "bold" } },
+            Pmenu = { bg = colors.crust, fg = "" },
+            PmenuSel = { bg = colors.surface0, fg = "" },
+            TelescopeSelection = { bg = colors.surface0 },
+            TelescopePromptCounter = { fg = colors.mauve, style = { "bold" } },
+            TelescopePromptPrefix = { bg = colors.surface0 },
+            TelescopePromptNormal = { bg = colors.surface0 },
+            TelescopeResultsNormal = { bg = colors.mantle },
+            TelescopePreviewNormal = { bg = colors.crust },
+            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+            TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+            TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
+            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
+            TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
+            IndentBlanklineChar = { fg = colors.surface0 },
+            IndentBlanklineContextChar = { fg = colors.surface2 },
+            GitSignsChange = { fg = colors.peach },
+            NvimTreeIndentMarker = { link = "IndentBlanklineChar" },
+            NvimTreeExecFile = { fg = colors.text },
+        }
+    end,
     integrations = {
         cmp = true,
         gitsigns = true,
@@ -49,4 +103,4 @@ catppuccin.setup({
 })
 
 -- setup must be called before loading
-vim.cmd.colorscheme "catppuccin-macchiato"
+vim.cmd.colorscheme "catppuccin-mocha"
